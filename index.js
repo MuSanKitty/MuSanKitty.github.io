8e5391a94bf00a92712bbf3a4a413fe42590474c
MuSanKitty <purpleshippo@gmail.com>
2017-05-16 11:52:14 -0400

    add slick and some template css
    Slick = jQuery carousel slider

--- index.js
+++ index.js
@@ -7,21 +7,8 @@ $('input[type="submit"]').mouseup(function(){
   $(this).css('background', '#1abc9c');
 });
 
-$('#loginform').click(function(){
-  $('.login').fadeToggle('slow');
+$('#loginForm').click(function(){
+  $('#login').fadeToggle('slow');
   $(this).toggleClass('green');
 });
 
-
-
-$(document).mouseup(function (e)
-{
-    var container = $(".login");
-
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.hide();
-        $('#loginform').removeClass('green');
-    }
-});
